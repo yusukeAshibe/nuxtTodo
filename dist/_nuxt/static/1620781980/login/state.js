8932d9fdc20dad5a842a39b65ef2ca2351b3c531
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jp  git config --list git config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2021-05-12 10:14:15 +0900

    generate
    a Please enter the commit message for your changes. Lines starting

---
+++ dist/_nuxt/static/1620781980/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620781980",layout:"default",error:b,state:{todo:{todos:[]},user:{id:a,name:a,token:a,pic:a,timestamp:0}},serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:b}},apollo:{defaultClient:Object.create(null)}}}("",null));
